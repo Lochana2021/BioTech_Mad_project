96f9eb244efb7cdc02bb87162e7b0882f9f53f68
AchiniHewawi <achinihewawitharana@gmail.com>
2021-04-23 13:09:16 +0530

    comment test -achi

--- app/src/main/java/com/example/biotechgeneral/MainActivity.java
+++ app/src/main/java/com/example/biotechgeneral/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //git test comment_ achi
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

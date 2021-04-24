d8bd9199649ee49d7a76b7f9d88a3230c2516f85
AchiniHewawi <achinihewawitharana@gmail.com>
2021-04-24 12:42:18 +0530

    ViewQuiz completed and all commit

--- app/src/main/java/com/example/biotechgeneral/ViewQuiz.java
+++ app/src/main/java/com/example/biotechgeneral/ViewQuiz.java
@@ -3,6 +3,9 @@ package com.example.biotechgeneral;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
 
 public class ViewQuiz extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class ViewQuiz extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_quiz);
+
+        Intent getIntentQuizListActivity = getIntent();
+    }
+
+    public void gotoPassListActivity (View view) {
+
+        Intent intent2 = new Intent(this, PassList.class);
+        startActivity(intent2);
+
+        Toast.makeText(getApplicationContext(), "Navigating....", Toast.LENGTH_SHORT).show();
     }
 }

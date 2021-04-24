8800e218a1b7526358278361a756410baedbf277
AchiniHewawi <achinihewawitharana@gmail.com>
2021-04-24 12:14:54 +0530

    QuizList completed and ViewQuiz created

--- app/src/main/java/com/example/biotechgeneral/QuizList.java
+++ app/src/main/java/com/example/biotechgeneral/QuizList.java
@@ -2,7 +2,10 @@ package com.example.biotechgeneral;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class QuizList extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class QuizList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_list);
+
+        Intent getIntentMainActivity = getIntent();
+    }
+
+    public void gotoViewQuizActivity (View view) {
+
+        Intent intent2 = new Intent(this, ViewQuiz.class);
+        startActivity(intent2);
+
+        Toast.makeText(getApplicationContext(), "Navigating....", Toast.LENGTH_SHORT).show();
+    }
+
+    public void gotoCreateQuizActivity (View view) {
+
+        Intent intent = new Intent(this, CreateQuiz.class);
+        startActivity(intent);
+
+        Toast.makeText(getApplicationContext(), "Navigating....", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/example/biotechgeneral/ViewQuiz.java
@@ -0,0 +1,14 @@
+package com.example.biotechgeneral;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewQuiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_quiz);
+    }
+}

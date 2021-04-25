0c6cf5640e66c59d0a01ecddcad8406c0dab7c97
Ashan Madhusanka <ashanmadhusanka339@gmail.com>
2021-04-25 12:09:16 +0530

    Forum part

---
+++ app/src/main/java/com/example/biotechgeneral/CreateForum.java
@@ -0,0 +1,14 @@
+package com.example.biotechgeneral;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateForum extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_forum);
+    }
+}

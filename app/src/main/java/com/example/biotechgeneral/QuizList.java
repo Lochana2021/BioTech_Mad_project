0bc77fcc8fa353518f6b475e9171e9de684d3970
AchiniHewawi <achinihewawitharana@gmail.com>
2021-04-26 19:36:42 +0530

    ListView SUCCESS

--- app/src/main/java/com/example/biotechgeneral/QuizList.java
+++ app/src/main/java/com/example/biotechgeneral/QuizList.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -45,27 +46,6 @@ public class QuizList extends AppCompatActivity {
 
         // Associate the quiz Firebase Database Reference with the database's quiz object
         dbRef = FirebaseDatabase.getInstance().getReference();
-                //.child("QuizClass").child("2");
-
-
-        /*
-        dbRef.child("QuizClass")
-                .orderByChild("quizNo")
-                .addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        for (snapshot childSnapshot : snapshot.getChildren()){
-                            String quizNoKeyValue = childSnapshot.getValue();
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-
-                    }
-                });
-    */
-
 
         // Attach a ChildEventListener to the quiz database, so we can retrieve the quiz entries
         dbRef.child("QuizClass").addChildEventListener(new ChildEventListener() {
@@ -73,42 +53,39 @@ public class QuizList extends AppCompatActivity {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
 
-
                 // Get the value from the DataSnapshot and add it to the quiz' list
                 String quizMapped = snapshot.child("quizNo").getValue(String.class);
-                quizArrayList.add(quizMapped);
+                quizArrayList.add("Quiz "+quizMapped);
 
                 // Notify the ArrayAdapter that there was a change
                 quizArrayAdapter.notifyDataSetChanged();
-
-                /*
-                QuizClass quizObject = (QuizClass) snapshot.getValue(QuizClass.class);
-                String quizString = String.valueOf(quizObject);
-                quizArrayAdapter.add(quizString);
-                */
             }
 
             @Override
             public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
                 quizArrayAdapter.notifyDataSetChanged();
             }
-
             @Override
             public void onChildRemoved(@NonNull DataSnapshot snapshot) {
 
             }
-
             @Override
             public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
 
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
             }
         } );
 
+        // CLick on ListView items to navigate
+        quizListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
 
     }// onCreate ends
 

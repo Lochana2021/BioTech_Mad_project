b730315efb4e79a5641caae07737728fdbbfb4c7
Lochana2021 <it19971490@my.sllit.lk>
2021-05-08 15:01:51 +0700

    error

--- app/src/main/java/com/example/biotechgeneral/ass_teacher.java
+++ app/src/main/java/com/example/biotechgeneral/ass_teacher.java
@@ -125,6 +125,7 @@ public class ass_teacher extends AppCompatActivity {
             }
         });//nav end
     }
+    //test
 
    /* public void goToTypeAct(View view){
         Intent i = new Intent(this,sigle_type.class);
